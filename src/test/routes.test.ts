7f385a5e2afa8bc08a926b3811bcee4f86ffa3b5
Jean Kevin <rasen414@gmail.com>
2019-02-17 23:52:07 +0100

    ajout du post user

--- src/test/routes.test.ts
+++ src/test/routes.test.ts
@@ -4,6 +4,8 @@ import request from 'supertest'
 import {models} from "../models"
 
 const chai = require('chai');
+let chaiJsonEqual = require('chai-json-equal');
+chai.use(chaiJsonEqual);
 const should = chai.should();
 
 //body-parser for parsing req.body
@@ -13,9 +15,9 @@ app.use(bodyParser.urlencoded({
 }));
 
 //describe() declares a test suite
-describe('GET routes', () => {
+describe('Users Test case', () => {
 
-    describe('index', function() {
+    describe('GET users', function() {
         this.timeout(60000);
 
         before((done) => {
@@ -24,14 +26,52 @@ describe('GET routes', () => {
             });
         });
 
-        it('should be status 200', (done) => {
+        it('should return one user', (done) => {
             request(app)
                 .get('/v1/users')
                 .end((err, res) => {
                     if (err) done(err);
                     res.status.should.equal(200);
+                    res.body[0].should.jsonEqual({"username": "toto", "email": "toto@toto.fr"});
                     done();
                 });
         });
+
+        after( (done) => {
+            models.users.destroy({
+                where: {},
+                truncate: true
+            }).then(value => {
+                done();
+            })
+        })
+    });
+
+    describe('POST users', function() {
+        this.timeout(60000);
+
+        it('should insert user in BDD', (done) => {
+            request(app)
+                .post('/v1/users')
+                .send({"username": "arthur", "email": "gigolo@carotte.fr"})
+                .end((err, res) => {
+                    if (err) done(err);
+                    models.users.findOne({ where: {email: 'gigolo@carotte.fr'} }).then(value => {
+                        if(value == null) {
+                            done("l'utilisateur n'a pas été enregistré en bdd")
+                        }
+                        done();
+                    });
+                });
+        });
+
+        after( (done) => {
+            models.users.destroy({
+                where: {},
+                truncate: true
+            }).then(value => {
+                done();
+            })
+        })
     });
 });
