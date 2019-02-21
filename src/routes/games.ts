6173455e8abd38fccc6373756326c17d1612c08b
Jean Kevin <rasen414@gmail.com>
2019-02-21 23:29:01 +0100

    add topics OK

--- src/routes/games.ts
+++ src/routes/games.ts
@@ -4,6 +4,7 @@ import {Roles} from "../common/roles";
 import * as common from "./common";
 import {checkId} from "./common";
 import {UriginError} from "../common/UriginError";
+import {UniqueConstraintError} from "sequelize";
 
 let router  = express.Router();
 
@@ -24,9 +25,26 @@ router.post('/:gameId/comments', common.isAuthenticate(), function(req,res){
     return common.post(models.comments)(req,res);
 });
 
-router.post('/:gameId/types',common.isAuthenticate(),common.checkRole(Roles.CREATOR),common.checkId('gameId',models.games,{model: models.users, as:'users'}),function (request, response) {
-    request.body.gameId = request.params.gameId;
-    return common.post(models.types)(request,response);
+router.post('/:gameId/types',common.isAuthenticate(),common.checkRole(Roles.CREATOR),common.checkId('gameId',models.games,{model: models.users, as:'creator'}),function (request, response) {
+    models.games.findByPk(request.params.gameId).then(game => {
+        if (game == null){
+            response.status(404).json({description: UriginError.OBJECT_NOT_FOUND});
+            return;
+        }
+        models.types.findByPk(request.body.typeId).then(function (type) {
+            if (type == null) response.status(500).json({description: UriginError.ERROR_WITH_DATABASE});
+            else {
+                game.addTypes(type);
+                response.json("OK");
+            }
+        }).catch(err => {
+            if (err instanceof UniqueConstraintError) {
+                response.status(409).json({description: UriginError.OBJECT_ALREADY_EXIST});
+            } else {
+                response.status(500).json({description: UriginError.ERROR_WITH_DATABASE});
+            }
+        });
+    });
 });
 
 router.put('/:gameId',common.filterBody({"description": "true"}),common.isAuthenticate(), common.checkRole(Roles.CREATOR), common.checkId('gameId',models.games,{model: models.users, as:'creator'}) , common.putByPk(models.games,"gameId"));
