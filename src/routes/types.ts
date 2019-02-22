dff1f22117ffa2e25be396b3bffca8c82523b2ae
Jean Kevin <rasen414@gmail.com>
2019-02-22 13:09:37 +0100

    correction type

--- src/routes/types.ts
+++ src/routes/types.ts
@@ -6,12 +6,12 @@ import * as common from "./common";
 let router  = express.Router();
 
 // Get de type
-router.get('/', common.get(models.types,["name", "description","createdAt","updatedAt"]));
-router.get('/:id',common.getByPk('id',models.types,["name", "description","createdAt","updatedAt"]));
+router.get('/', common.get(models.types,["id","name", "description","createdAt","updatedAt"]));
+router.get('/:typeId',common.getByPk('typeId',models.types,["id","name", "description","createdAt","updatedAt"]));
 // Post de type
 router.post('/',common.isAuthenticate(),common.checkRole(Roles.CREATOR),common.post(models.types));
 // Put de type
 router.put('/:typeId',common.isAuthenticate(),common.checkRole(Roles.CREATOR),common.putByPk(models.types,"typeId"));
 // Delete de type
-router.delete('/:id',common.isAuthenticate(),common.checkRole(Roles.CREATOR),common.deleteFunc(models.types,{id:"id"}));
+router.delete('/:typeId',common.isAuthenticate(),common.checkRole(Roles.CREATOR),common.deleteFunc(models.types,{id:"typeId"}));
 export default router;
